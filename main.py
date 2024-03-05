1e27a8c6b2396ebbee0bb44d9d98a885f0706875
stud4 <stud4@gmail.com>
2024-03-05 08:53:02 -0500

    player_intro.py -> main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from player_intro import intro
+from player_intro import player_name
+
+#introduction
+intro()
+player_name()
---
+++ player_intro.py
@@ -0,0 +1,8 @@
+#forgotten realms intro
+def intro():
+    print("Welcome to the Forgotten Realms, a vast and diverse fantasy world filled with magic, mythical creatures, and ancient lore.\nAs you embark on your adventure, prepare to explore intricate cities, delve into forgotten dungeons, and encounter a rich tapestry of cultures that shape this enchanting realm.")
+
+#player_name
+def player_name():
+    player_name = input("What is your name traveler? ").title()
+    print("Welcome to the Forgotten Realms " + player_name + "!")
