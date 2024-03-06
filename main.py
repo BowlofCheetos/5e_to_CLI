db8add1d27a979cc40324b3cb2cd13eacf997a1b
stud4 <stud4@gmail.com>
2024-03-06 13:35:20 -0500

    create modifier_assign.py and called in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from global_vars import *
 from player_intro import *
 from race_info import *
 from score_assign import *
+from modifier_assign import *
 
 #introduction
 intro()
@@ -9,6 +10,20 @@ player_name()
 
 #score selection
 score_assign()
+
+#modifier generation
+mod_assign(str_score)
+str_mod = modifier_output
+mod_assign(dex_score)
+dex_mod = modifier_output
+mod_assign(con_score)
+con_mod = modifier_output
+mod_assign(int_score)
+int_mod = modifier_output
+mod_assign(wis_score)
+wis_mod = modifier_output
+mod_assign(cha_score)
+cha_mod = modifier_output
 
 #race info
 race_info()
