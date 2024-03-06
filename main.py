070d20c3aab0f01d381b6da0764c87d59ac585df
stud4 <stud4@gmail.com>
2024-03-06 12:56:41 -0500

    created score assign and pushed to main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from global_vars import *
 from player_intro import *
 from race_info import *
+from score_assign import *
 
 #introduction
 intro()
 player_name()
 
 #score selection
-
+score_assign()
 
 #race info
 race_info()
