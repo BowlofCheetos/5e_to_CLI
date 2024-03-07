0919f9e103ee18df6e7dc2e6d758af2ef4731b57
stud4 <stud4@gmail.com>
2024-03-07 07:55:31 -0500

    created skills.py

---
+++ skills.py
@@ -0,0 +1,28 @@
+from global_vars import *
+
+#skill list
+athletics = 'Athletics'
+acrobatics = 'Acrobatics'
+arcana = 'Arcana'
+animal_handling = 'Animal Handling'
+deception = 'Deception'
+history = 'History'
+insight = 'Insight'
+intimidation = 'Intimidation'
+investigation = 'Investigation'
+medicine = 'Medicine'
+nature = 'Nature'
+perception = 'Perception'
+performance = 'Performance'
+persuasion = 'Persuasion'
+religion = 'Religion'
+sleight_of_hand = 'Sleight of Hand'
+stealth = 'Stealth'
+survival = 'Survival'
+
+#skills by score
+str_skills = [athletics]
+dex_skills = [acrobatics, sleight_of_hand, stealth]
+int_skills = [arcana, history, investigation, nature, religion]
+wis_skills = [animal_handling, insight, medicine, perception, survival]
+cha_skills = [deception, performance, persuasion]
